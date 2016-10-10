03a29b3475b85f2ae2db41b9d2ae12812cee7428
Harikrishnan <hari.krishnan@razorthink.com>
2016-10-09 20:31:01 -0700

    Additional changes

--- src/main/java/com/codeoccult/syncapp/Application.java
+++ src/main/java/com/codeoccult/syncapp/Application.java
@@ -6,10 +6,12 @@ package com.codeoccult.syncapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
     }
 }
--- src/main/java/com/codeoccult/syncapp/controllers/RoutesConfig.java
+++ src/main/java/com/codeoccult/syncapp/controllers/RoutesConfig.java
@@ -1,5 +1,9 @@
 package com.codeoccult.syncapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RoutesConfig {
+    @Autowired
+    private ApplicationContext ctx;
+
     @RequestMapping("/greeting")
     public String helloworld(){
         return "Hello World";
     }
+
+   /* @RequestMapping("/shutdown")
+    public void shutdown(){
+        ((ConfigurableApplicationContext) ctx).close();
+    }
+    */
 }
